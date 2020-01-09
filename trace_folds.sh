871d5d858e00b33d8d178fc8ebd3a22779e659a9
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-01-09 21:26:51 +0200

    roberta

--- trace_folds.sh
+++ trace_folds.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-CONF=configs/roberta_base_with_cfs.yml
+
+CONF=configs/roberta_large_with_cfs.yml
 INPUT_TYPE='roberta-categories-stats'
-LOG_DIR_PREFIX=logs/folds/roberta_base_cfs
-ONAME_PATTERN=rb_bcfs
+LOG_DIR_PREFIX=logs/folds/roberta_large_cfs
+ONAME_PATTERN=rb_lcfs
 
-CHECKPOINT=${LOG_DIR_PREFIX}_00/checkpoints/stage4.19.pth  # 0.3904
-OUTPUT=${ONAME_PATTERN}_00.pt
+CHECKPOINT=${LOG_DIR_PREFIX}_0_1/checkpoints/best.pth  # 0.3909
+OUTPUT=${ONAME_PATTERN}_0.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_01/checkpoints/stage1.7.pth  # 0.3773
-OUTPUT=${ONAME_PATTERN}_01.pt
+CHECKPOINT=${LOG_DIR_PREFIX}_1_1/checkpoints/best.pth  # 0.3878
+OUTPUT=${ONAME_PATTERN}_1.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_02/checkpoints/stage4.15.pth  # 0.3993
-OUTPUT=${ONAME_PATTERN}_02.pt
+CHECKPOINT=${LOG_DIR_PREFIX}_2_1/checkpoints/best.pth  # 0.3900
+OUTPUT=${ONAME_PATTERN}_2.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_03/checkpoints/stage2.9.pth  # 0.3879
-OUTPUT=${ONAME_PATTERN}_03.pt
-python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
+# CHECKPOINT=${LOG_DIR_PREFIX}_03/checkpoints/stage2.9.pth  # 0.3879
+# OUTPUT=${ONAME_PATTERN}_03.pt
+# python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_04/checkpoints/stage2.9.pth  # 0.3810
-OUTPUT=${ONAME_PATTERN}_04.pt
-python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
+# CHECKPOINT=${LOG_DIR_PREFIX}_04/checkpoints/stage2.9.pth  # 0.3810
+# OUTPUT=${ONAME_PATTERN}_04.pt
+# python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 echo "Traced all models based on config - ${CONF}."
 
 ARCHIVE_NAME=${ONAME_PATTERN}.zip
