70cda277e919d7a5c3fee768444c8326eaf7e880
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-02-11 09:37:57 +0200

    folds data

--- trace_folds.sh
+++ trace_folds.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-CONF=configs/bert_base_uncased_with_stats_and_te.yml
-INPUT_TYPE='transformers-stats'
-LOG_DIR_PREFIX=logs/folds/bert_base_uncased_stats_and_te
-ONAME_PATTERN=bb_u_ste
 
-CHECKPOINT=${LOG_DIR_PREFIX}_00/checkpoints/stage4.13.pth  # 0.3948
-OUTPUT=${ONAME_PATTERN}_00.pt
+CONF=configs/xlnet_large_cased.yml
+INPUT_TYPE='transformers'
+LOG_DIR_PREFIX=logs/folds/xlnet_large_cased
+ONAME_PATTERN=xlc_ls_3f
+
+CHECKPOINT=${LOG_DIR_PREFIX}_0_1/checkpoints/best.pth  # 0.3907
+OUTPUT=${ONAME_PATTERN}_0.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_01/checkpoints/stage4.12.pth  # 0.3800
-OUTPUT=${ONAME_PATTERN}_01.pt
+CHECKPOINT=${LOG_DIR_PREFIX}_1_1/checkpoints/best.pth  # 0.3914
+OUTPUT=${ONAME_PATTERN}_1.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_02/checkpoints/stage1.5.pth  # 0.3996
-OUTPUT=${ONAME_PATTERN}_02.pt
+CHECKPOINT=${LOG_DIR_PREFIX}_2_1/checkpoints/best.pth  # 0.3829
+OUTPUT=${ONAME_PATTERN}_2.pt
 python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_03/checkpoints/stage2.7.pth  # 0.3929
-OUTPUT=${ONAME_PATTERN}_03.pt
-python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
+# CHECKPOINT=${LOG_DIR_PREFIX}_03/checkpoints/stage3.12.pth  # 0.3840
+# OUTPUT=${ONAME_PATTERN}_03.pt
+# python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
 
-CHECKPOINT=${LOG_DIR_PREFIX}_04/checkpoints/stage2.7.pth  # 0.3837
-OUTPUT=${ONAME_PATTERN}_04.pt
-python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
+# CHECKPOINT=${LOG_DIR_PREFIX}_04/checkpoints/stage3.11.pth  # 0.3744
+# OUTPUT=${ONAME_PATTERN}_04.pt
+# python3 -m src.trace --config ${CONF} --state ${CHECKPOINT} --input-type ${INPUT_TYPE} --out ${OUTPUT}
+
 echo "Traced all models based on config - ${CONF}."
 
-# ARCHIVE_NAME=${ONAME_PATTERN}.zip
-# zip -r ${ARCHIVE_NAME} ${ONAME_PATTERN}_*
-# echo "Compressed to archive - ${ARCHIVE_NAME}."
+ARCHIVE_NAME=${ONAME_PATTERN}.zip
+zip -r ${ARCHIVE_NAME} ${ONAME_PATTERN}_*
+echo "Compressed to archive - ${ARCHIVE_NAME}."
